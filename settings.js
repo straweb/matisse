921ac5e396d34209a597cb515c62c02c24101365
aaruna <aaruna.g@imaginea.com>
2013-07-08 12:11:11 +0530

    Added multiple users in settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,8 @@
 module.exports = {
     appName: 'matisse',
-    email: 'admin@pramati.com',
-    password: 'pramati123',
-    user: "admin",
+    users: {'admin@pramati.com': {email: 'admin@pramati.com', password: 'pramati123', user: 'admin'},
+            'user@pramati.com': {email: 'user@pramati.com', password: 'pramati123', user: 'user'},
+            'user2@pramati.com': {email: 'user2@pramati.com', password: 'pramati123', user: 'user2'}
+    },
     serverPort : 8000
 };
